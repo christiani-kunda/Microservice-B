bbf9032e61952d2283089a0364305da7c3d793f1
christiani_kunda <c.iradukunda@irembo.com>
2019-12-17 13:09:00 +0200

    fixed the tests

--- src/test/java/com/christian/service/RestServerTests.java
+++ src/test/java/com/christian/service/RestServerTests.java
@@ -1,4 +1,4 @@
-package rw.irembo.authentication;
+package com.christian.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IremboAuthenticationGatewayApplicationTests {
+public class ResteServerTests {
 
 	@Test
 	public void contextLoads() {
